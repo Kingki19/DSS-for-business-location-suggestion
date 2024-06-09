8e5740f28ecc8b6444329e0ec8f06bcd82caae9d
Kingki19 || Muhammad Rizqi <114461022+Kingki19@users.noreply.github.com>
2024-06-10 06:35:43 +0700

    Update topsis.py

--- pages/topsis.py
+++ pages/topsis.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from scipy.stats import rankdata
 
 # KONSTANTA
 COLUMN_EXCLUDE = 'alternatif'
